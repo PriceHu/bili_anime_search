8327b60defbb30f51e054f62aa6463f94e278ad0
PriceHu <price.hu237388964@live.com>
2021-10-30 20:50:24 -0400

    change packing script build dir

--- pack_ios.sh
+++ pack_ios.sh
@@ -1,7 +1,7 @@
 mkdir Payload
-cp -r ./build/ios/iphoneos/Runner.app ./Payload
-zip -r ./build/ios/iphoneos/Payload.zip ./Payload
-mv ./build/ios/iphoneos/Payload.zip ./build/ios/iphoneos/faith.uchidakotori.biliAnimeSearch.ipa
+cp -r ./build/ios/Release-iphoneos/Runner.app ./Payload
+zip -r ./build/ios/Release-iphoneos/Payload.zip ./Payload
+mv ./build/ios/Release-iphoneos/Payload.zip ./build/ios/Release-iphoneos/faith.uchidakotori.biliAnimeSearch.ipa
 rm -r ./Payload
 
-echo "ipa packed to ./build/ios/iphoneos/faith.uchidakotori.biliAnimeSearch.ipa"
+echo "ipa packed to ./build/ios/Release-iphoneos/faith.uchidakotori.biliAnimeSearch.ipa"
